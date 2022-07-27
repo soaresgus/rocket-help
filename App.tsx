412389a101c552a8eadb8d20a3557e57cd334d33
soaresgus <gustavosoaresbt@hotmail.com>
2022-07-27 11:58:46 -0300

    Call home screen on app

--- App.tsx
+++ App.tsx
@@ -1,25 +1,31 @@
+import React from 'react';
+
 import {
   Roboto_400Regular,
   Roboto_700Bold,
   useFonts,
 } from '@expo-google-fonts/roboto';
+
 import { NativeBaseProvider, StatusBar } from 'native-base';
-import React from 'react';
+
 import { Loading } from './src/components/Loading';
+
 import { SignIn } from './src/screens/SignIn';
+import { Home } from './src/screens/Home';
+import { THEME } from './src/styles/theme';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME}>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
 
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      {fontsLoaded ? <Home /> : <Loading />}
     </NativeBaseProvider>
   );
 }
