0b5a8cc0105327b48ce9ff521a7ad10ba617cda1
soaresgus <gustavosoaresbt@hotmail.com>
2022-07-27 11:58:36 -0300

    Create home screen

---
+++ src/screens/Home.tsx
@@ -0,0 +1,85 @@
+import {
+  HStack,
+  VStack,
+  IconButton,
+  useTheme,
+  Text,
+  Heading,
+  FlatList,
+} from 'native-base';
+
+import { SignOut } from 'phosphor-react-native';
+import { useState } from 'react';
+
+import Logo from '../assets/logo_secondary.svg';
+
+import { Filter } from '../components/Filter';
+import { Order, IOrder } from '../components/Order';
+
+export function Home() {
+  const { colors } = useTheme();
+
+  const [statusSelected, setStatusSelected] = useState<'open' | 'closed'>(
+    'open'
+  );
+  const [orders, setOrders] = useState<IOrder[]>([
+    {
+      id: '123',
+      patrimony: '14326',
+      when: '18/08/2022 - 10:00',
+      status: 'open',
+    },
+  ]);
+
+  return (
+    <VStack flex={1} pb={6} bg="gray.700">
+      <HStack
+        w="full"
+        justifyContent="space-between"
+        alignItems="center"
+        bg="gray.600"
+        pt={12}
+        pb={5}
+        px={6}
+      >
+        <Logo />
+
+        <IconButton icon={<SignOut size={26} color={colors.gray[300]} />} />
+      </HStack>
+
+      <VStack flex={1} px={6}>
+        <HStack
+          w="full"
+          mt={8}
+          mb={4}
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Heading color="gray.100">Meus chamados</Heading>
+          <Text color="gray.200">3</Text>
+        </HStack>
+
+        <HStack space={3} mb={8}>
+          <Filter
+            type="open"
+            title="Em andamento"
+            onPress={() => setStatusSelected('open')}
+            isActive={statusSelected === 'open'}
+          />
+          <Filter
+            type="closed"
+            title="Finalizados"
+            onPress={() => setStatusSelected('closed')}
+            isActive={statusSelected === 'closed'}
+          />
+        </HStack>
+
+        <FlatList
+          data={orders}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <Order data={item} />}
+        />
+      </VStack>
+    </VStack>
+  );
+}
