13fc43d02f90266f49c06762b00f555e0e812f56
soaresgus <gustavosoaresbt@hotmail.com>
2022-07-28 07:52:17 -0300

    Change order component to clickable

--- src/components/Order.tsx
+++ src/components/Order.tsx
@@ -1,4 +1,13 @@
-import { Box, Circle, HStack, Text, useTheme, VStack } from 'native-base';
+import {
+  Box,
+  Circle,
+  HStack,
+  Text,
+  useTheme,
+  VStack,
+  Pressable,
+  IPressableProps,
+} from 'native-base';
 import {
   CircleWavyCheck,
   ClockAfternoon,
@@ -9,10 +18,10 @@ export interface IOrder {
   id: string;
   patrimony: string;
   when: string;
-  status: 'open' | 'close';
+  status: 'open' | 'closed';
 }
 
-interface OrderProps {
+interface OrderProps extends IPressableProps {
   data: IOrder;
 }
 
@@ -22,36 +31,38 @@ export function Order({ data, ...props }: OrderProps) {
     data.status === 'open' ? colors.secondary[700] : colors.green[300];
 
   return (
-    <HStack
-      bg="gray.600"
-      mb={4}
-      alignItems="center"
-      justifyContent="space-between"
-      rounded="sm"
-      overflow="hidden"
-    >
-      <Box h="full" w={2} bg={statusColor} />
+    <Pressable {...props}>
+      <HStack
+        bg="gray.600"
+        mb={4}
+        alignItems="center"
+        justifyContent="space-between"
+        rounded="sm"
+        overflow="hidden"
+      >
+        <Box h="full" w={2} bg={statusColor} />
 
-      <VStack flex={1} my={5} ml={5}>
-        <Text color="white" fontSize="md">
-          Patrimônio {data.patrimony}
-        </Text>
-
-        <HStack alignItems="center">
-          <ClockAfternoon size={15} color={colors.gray[300]} />
-          <Text color="gray.200" fontSize="xs" ml={1}>
-            {data.when}
+        <VStack flex={1} my={5} ml={5}>
+          <Text color="white" fontSize="md">
+            Patrimônio {data.patrimony}
           </Text>
-        </HStack>
-      </VStack>
 
-      <Circle bg="gray.500" h={12} w={12} mr={5}>
-        {data.status === 'close' ? (
-          <CircleWavyCheck size={24} color={statusColor} />
-        ) : (
-          <Hourglass size={24} color={statusColor} />
-        )}
-      </Circle>
-    </HStack>
+          <HStack alignItems="center">
+            <ClockAfternoon size={15} color={colors.gray[300]} />
+            <Text color="gray.200" fontSize="xs" ml={1}>
+              {data.when}
+            </Text>
+          </HStack>
+        </VStack>
+
+        <Circle bg="gray.500" h={12} w={12} mr={5}>
+          {data.status === 'closed' ? (
+            <CircleWavyCheck size={24} color={statusColor} />
+          ) : (
+            <Hourglass size={24} color={statusColor} />
+          )}
+        </Circle>
+      </HStack>
+    </Pressable>
   );
 }
