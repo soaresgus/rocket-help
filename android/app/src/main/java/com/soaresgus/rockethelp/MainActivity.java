724490df6a65ed95ec7f93d0066163fcf8f3afa8
soaresgus <gustavosoaresbt@hotmail.com>
2022-07-28 11:50:26 -0300

    Configure firebase conection on project

--- android/app/src/main/java/com/soaresgus/rockethelp/MainActivity.java
+++ android/app/src/main/java/com/soaresgus/rockethelp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.rockethelp;
+package com.soaresgus.rockethelp;
 
 import android.os.Build;
 import android.os.Bundle;
--- android/app/src/main/java/com/soaresgus/rockethelp/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate.java
+++ android/app/src/main/java/com/soaresgus/rockethelp/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate.java
@@ -1,4 +1,4 @@
-package com.rockethelp.newarchitecture.modules;
+package com.soaresgus.rockethelp.newarchitecture.modules;
 
 import com.facebook.jni.HybridData;
 import com.facebook.react.ReactPackage;
