3af253c40a275becb73c9dc3ccac39e30cfa0f24
soaresgus <gustavosoaresbt@hotmail.com>
2022-07-28 10:31:51 -0300

    Get route param on details screen

--- src/screens/Details.tsx
+++ src/screens/Details.tsx
@@ -1,10 +1,20 @@
-import { VStack } from 'native-base';
+import { Text, VStack } from 'native-base';
 import { Header } from '../components/Header';
 
+import { useRoute } from '@react-navigation/native';
+
+interface RouteParams {
+  orderId: string;
+}
+
 export function Details() {
+  const route = useRoute();
+  const { orderId } = route.params as RouteParams;
+
   return (
     <VStack flex={1} bg="gray.700">
       <Header title="Solicitação" />
+      <Text color="white">{orderId}</Text>
     </VStack>
   );
 }
