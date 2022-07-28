6b5c0ebe55c0f2c8f7faf2be6a5c427c568c61bf
soaresgus <gustavosoaresbt@hotmail.com>
2022-07-28 10:32:11 -0300

    Create navigate to details route on home

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -26,12 +26,23 @@ export function Home() {
   const [statusSelected, setStatusSelected] = useState<'open' | 'closed'>(
     'open'
   );
-  const [orders, setOrders] = useState<IOrder[]>();
+  const [orders, setOrders] = useState<IOrder[]>([
+    {
+      id: '123',
+      patrimony: '212346',
+      when: '18/07/2022 às 14:00',
+      status: 'open',
+    },
+  ]);
 
   const navigation = useNavigation();
 
   function handleNewOrder() {
     navigation.navigate('new');
+  }
+
+  function handleOpenDetails(orderId: string) {
+    navigation.navigate('details', { orderId });
   }
 
   return (
@@ -58,8 +69,8 @@ export function Home() {
           justifyContent="space-between"
           alignItems="center"
         >
-          <Heading color="gray.100">Meus chamados</Heading>
-          <Text color="gray.200">3</Text>
+          <Heading color="gray.100">Solicitações</Heading>
+          <Text color="gray.200">{orders.length}</Text>
         </HStack>
 
         <HStack space={3} mb={8}>
@@ -80,7 +91,9 @@ export function Home() {
         <FlatList
           data={orders}
           keyExtractor={(item) => item.id}
-          renderItem={({ item }) => <Order data={item} />}
+          renderItem={({ item }) => (
+            <Order data={item} onPress={() => handleOpenDetails(item.id)} />
+          )}
           showsVerticalScrollIndicator={false}
           contentContainerStyle={{ paddingBottom: 100 }}
           ListEmptyComponent={() => (
