8f5583e382f72a97b324e09f6ca1bc58df70fb5e
soaresgus <gustavosoaresbt@hotmail.com>
2022-07-29 09:33:41 -0300

    Create auth function

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 
+import { Alert } from 'react-native';
+
 import { VStack, Heading, Icon, useTheme } from 'native-base';
 
 import Logo from '../assets/logo_primary.svg';
@@ -9,11 +11,43 @@ import { Button } from '../components/Button';
 
 import { Envelope, Key } from 'phosphor-react-native';
 
+import auth from '@react-native-firebase/auth';
+
 export function SignIn() {
   const { colors } = useTheme();
 
-  const [email, setEmail] = useState<String>();
-  const [password, setPassword] = useState<String>();
+  const [email, setEmail] = useState<string>();
+  const [password, setPassword] = useState<string>();
+  const [isLoading, setIsLoading] = useState(false);
+
+  function handleSignIn() {
+    if (!email || !password) {
+      return Alert.alert('Login', 'Informe e-mail e senha.');
+    }
+
+    setIsLoading(true);
+
+    auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch((error) => {
+        console.log(error);
+        setIsLoading(false);
+
+        if (error.code === 'auth/invalid-email') {
+          return Alert.alert('Login', 'E-mail inválido');
+        }
+
+        if (error.code === 'auth/user-not-found') {
+          return Alert.alert('Login', 'E-mail ou senha inválida.');
+        }
+
+        if (error.code === 'auth/wrong-password') {
+          return Alert.alert('Login', 'E-mail ou senha inválida.');
+        }
+
+        return Alert.alert('Login', 'Não foi possível acessar');
+      });
+  }
 
   return (
     <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
@@ -39,7 +73,12 @@ export function SignIn() {
         onChangeText={setPassword}
       />
 
-      <Button title="Entrar" w="full" />
+      <Button
+        title="Entrar"
+        w="full"
+        onPress={handleSignIn}
+        isLoading={isLoading}
+      />
     </VStack>
   );
 }
