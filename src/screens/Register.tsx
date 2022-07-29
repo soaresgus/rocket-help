8f0f822d9388a69d73daa16daeec330f826f85f6
soaresgus <gustavosoaresbt@hotmail.com>
2022-07-29 10:15:15 -0300

    Register new orders on firestore

--- src/screens/Register.tsx
+++ src/screens/Register.tsx
@@ -1,14 +1,62 @@
+import { useState } from 'react';
+
+import { Alert } from 'react-native';
+
 import { VStack } from 'native-base';
+
 import { Button } from '../components/Button';
 import { Header } from '../components/Header';
 import { Input } from '../components/Input';
 
+import firestore from '@react-native-firebase/firestore';
+
+import { useNavigation } from '@react-navigation/native';
+
 export function Register() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [patrimony, setPatrimony] = useState<string>();
+  const [description, setDescription] = useState<string>();
+
+  const navigation = useNavigation();
+
+  function handleNewOrderRegister() {
+    if (!patrimony || !description) {
+      return Alert.alert('Nova solicitação', 'Preencha todos os campos');
+    }
+
+    setIsLoading(true);
+
+    firestore()
+      .collection('orders')
+      .add({
+        patrimony,
+        description,
+        status: 'open',
+        created_at: firestore.FieldValue.serverTimestamp(),
+      })
+      .then(() => {
+        Alert.alert('Nova solicitação', 'Solicitação registrada com sucesso.');
+        navigation.goBack();
+      })
+      .catch((error) => {
+        console.log(error);
+        setIsLoading(false);
+        return Alert.alert(
+          'Nova solicitação',
+          'Não foi possível criar uma nova solicitação.'
+        );
+      });
+  }
+
   return (
     <VStack flex={1} p={6} bg="gray.600">
       <Header title="Nova solicitação" />
 
-      <Input placeholder="Número do patrimônio" mt={4} />
+      <Input
+        placeholder="Número do patrimônio"
+        mt={4}
+        onChangeText={setPatrimony}
+      />
 
       <Input
         placeholder="Descrição do problema"
@@ -16,9 +64,15 @@ export function Register() {
         mt={5}
         multiline
         textAlignVertical="top"
+        onChangeText={setDescription}
       />
 
-      <Button title="Cadastrar" mt={5} />
+      <Button
+        title="Cadastrar"
+        mt={5}
+        isLoading={isLoading}
+        onPress={handleNewOrderRegister}
+      />
     </VStack>
   );
 }
